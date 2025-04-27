a9cffc3a8c52afb37304011115072bfbd0c1bd13
Luis David <lrambao5432@gmail.com>
2025-04-26 19:34:23 -0500

    first commit

---
+++ src/utils/screenNames.ts
@@ -0,0 +1,16 @@
+// Constants for screen names
+
+export const SCREEN_NAMES = {
+  Splash: "Splash",
+  Onboarding: "Onboarding",
+  Onboarding2: "Onboarding2",
+  Onboarding3: "Onboarding3",
+  SignIn: "SignIn",
+  SignUp: "SignUp",
+  CheckEmail: "CheckEmail",
+  ForgotPassword: "ForgotPassword",
+  ResetPassword: "ResetPassword",
+  Home: "Home",
+  Profile: "Profile",
+  Settings: "Settings",
+};
